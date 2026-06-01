fefef01e07966aa4899357e2e892c5fdbe1bc606
zeeshan2656 <zeeshanhaider2656@gmail.com>
2026-06-01 13:42:59 +0500

    ok

---
+++ config/database_example.php
@@ -0,0 +1,7 @@
+// Database Credentials
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
+define('DB_NAME', 'u434697879_isp');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8mb4');
